49038b4d157e8bd55967d13936782c75dfbf0920
unknown <takiluassefa1@gmail.com>
2024-04-03 19:45:09 +0300

    command wheelchair

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+#from tensorflow.keras import models
+
+import tensorflow as tf
+
+#from keras import models
+from recording_helper import record_audio, terminate
+from tf_helper import preprocess_audiobuffer
+
+from keras.models import load_model
+commands = ['stop', 'go', 'up', 'no', 'yes', 'right', 'left', 'down']
+
+loaded_model = tf.keras.models.load_model("saved_model")
+
+#loaded_model = tf.keras.layers.TFSMLayer("saved_model", call_endpoint='serving_default')
+
+
+def predict_mic():
+    audio = record_audio()
+    spec = preprocess_audiobuffer(audio)
+    prediction = loaded_model(spec)
+    label_pred = np.argmax(prediction, axis=1)
+    command = commands[label_pred[0]]
+    print("Predicted label:", command)
+    return command
+
+if __name__ == "__main__":
+    from turtle_helper import move_turtle
+    while True:
+        command = predict_mic()
+        move_turtle(command)
+        if command == "stop":
+            terminate()
+            break
