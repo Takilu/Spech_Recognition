09e11939f0d7cf453c87164a054f2b6804fbbb1f
unknown <takiluassefa1@gmail.com>
2024-06-09 20:47:04 +0300

    speech rec

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 #from tensorflow.keras import models
-
+#from keras import models
 import tensorflow as tf
 
 #from keras import models
@@ -9,17 +9,19 @@ from tf_helper import preprocess_audiobuffer
 
 from keras.models import load_model
 commands = ['stop', 'go', 'up', 'no', 'yes', 'right', 'left', 'down']
+print(tf.__version__)
+#loaded_model = models.load_model("saved_model.h5")
 
-loaded_model = tf.keras.models.load_model("saved_model")
+loaded_model = tf.keras.layers.TFSMLayer("saved_model", call_endpoint='serving_default')
+####################
 
-#loaded_model = tf.keras.layers.TFSMLayer("saved_model", call_endpoint='serving_default')
-
+###################
 
 def predict_mic():
     audio = record_audio()
     spec = preprocess_audiobuffer(audio)
     prediction = loaded_model(spec)
-    label_pred = np.argmax(prediction, axis=1)
+    label_pred = np.argmax(prediction, axis = 1)
     command = commands[label_pred[0]]
     print("Predicted label:", command)
     return command
@@ -32,3 +34,5 @@ if __name__ == "__main__":
         if command == "stop":
             terminate()
             break
+
+
